30ad11aa52d2d4d4ea4e943f4c5a930b59048c98
Ahmed Ashraf <ahmed29329@gmail.com>
2018-10-29 00:50:22 +0200

    support live preview mode

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"os/exec"
 	"strings"
 
 	"github.com/ahmedash95/hugo-dashboard/hugo"
@@ -16,17 +17,25 @@ import (
 var port *string
 var sitePath *string
 var contentDir *string
+var hugoBinPath *string
+var livePreviewPath string
 
 var Hugo *hugo.Hugo
 
 func main() {
 
+	livePreviewPath = "http://localhost:1313/"
+
 	port = flag.String("port", "9999", "Run dashboard of your website on speicifc port")
 	sitePath = flag.String("path", "", "hugo website directory path to serve")
 	contentDir = flag.String("content-dir", "content", "hugo content directory if not content")
+	hugoBinPath = flag.String("bin-path", "/usr/local/bin/hugo", "hugo bin file to run your website for live preview")
+
 	flag.Parse()
 
 	checkpath(*sitePath)
+
+	go runWebsite(*hugoBinPath)
 
 	hugo.Init(*sitePath, *contentDir)
 
@@ -44,11 +53,12 @@ func indexHandler(c *gin.Context) {
 	h := hugo.Get()
 	pages_list, _ := json.Marshal(h.GetPagesTree())
 	c.HTML(http.StatusOK, "dashboard.tmpl", gin.H{
-		"title":      h.Title,
-		"theme":      h.Theme,
-		"path":       h.Path,
-		"pages":      h.GetPages(),
-		"pages_list": string(pages_list),
+		"title":        h.Title,
+		"theme":        h.Theme,
+		"path":         h.Path,
+		"pages":        h.GetPages(),
+		"pages_list":   string(pages_list),
+		"live_preview": livePreviewPath,
 	})
 }
 
@@ -158,4 +168,12 @@ func createDirectoryHandler(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"msg": fmt.Sprintf("directory has been created to path %s", path),
 	})
+}
+
+func runWebsite(bin string) {
+	out, err := exec.Command(bin, "server", "--source", *sitePath).Output()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	fmt.Println(string(out))
 }
